326dd3bdd245673b3a47d32a1d962eb209e1cd2c
kunal singh <anandkunal0617@gmail.com>
2022-01-22 21:20:13 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ router.post("/register", async (req, res) => {
     email: req.body.email,
     password: CryptoJS.AES.encrypt(
       req.body.password,
-      process.env.SECRET_KEY
+      kunal
     ).toString(),
   });
 
@@ -28,7 +28,7 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(401).json("Wrong password or username");
 
-    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const bytes = CryptoJS.AES.decrypt(user.password, kunal);
     const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
 
     originalPassword !== req.body.password &&
@@ -36,7 +36,7 @@ router.post("/login", async (req, res) => {
 
     const accessToken = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
-      process.env.SECRET_KEY,
+      kunal,
       { expiresIn: "10d" }
     );
 
